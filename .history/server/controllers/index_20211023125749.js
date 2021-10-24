0db1d351d08b850b62e6617735fed2912777bbcf
UlkarZakaryayeva <uzakarya@my.centennialcollege.ca>
2021-10-23 22:27:19 -0400

    initial commit

---
+++ .history/server/controllers/index_20211023125749.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+let passport = require('passport');
+
+// create the User Model instance
+let userModel = require('../models/user');
+let User = userModel.User; // alias
+
+
+module.exports.displayHomePage = (req, res, next) => {
+    res.render('index', { title: 'Home' });
+}
+
+module.exports.displayAboutPage = (req, res, next) => {
+    res.render('about', { title: 'About Me' });
+}
+
+module.exports.displayProjectsPage = (req, res, next) => {
+    res.render('projects', { title: 'Projects' });
+}
+
+module.exports.displayServicesPage = (req, res, next) => {
+    res.render('services', { title: 'Services' });
+}
+
+module.exports.displayContactPage = (req, res, next) => {
+    res.render('contact', { title: 'Contact Me' });
+}
---
+++ .history/server/routes/index_20211023144741.js
@@ -0,0 +1,46 @@
+/* File Name: index.js
+   Name: Ulkar Zakaryayeva
+   Student Number: 301107604
+   Date: October 4, 2021
+*/
+
+
+let express = require('express');
+let router = express.Router();
+
+let indexController = require('../controllers/index');
+
+/* GET Home page. */
+router.get('/', indexController.displayHomePage);
+
+/* GET Home page. */
+router.get('/home', indexController.displayHomePage);
+
+/* GET About Me page. */
+router.get('/about', indexController.displayAboutPage);
+
+/* GET Projects page. */
+router.get('/projects', indexController.displayProjectsPage);
+
+/* GET Services page. */
+router.get('/services', indexController.displayServicesPage);
+
+/* GET Contact Me page. */
+router.get('/contact', indexController.displayContactPage);
+
+/* GET Route for displaying the Login page */
+router.get('/login', contactController.displayLoginPage);
+
+/* POST Route for processing the Login page */
+router.post('/login', contactController.processAddPage);
+
+/* GET Route for displaying the Register page */
+router.get('/register', contactController.displayAddPage);
+
+/* POST Route for processing the Register page */
+router.post('/register', contactController.processAddPage);
+
+/* GET to perform the UserLogout */
+router.get('/logout', contactController.performDelete);
+
+module.exports = router;
